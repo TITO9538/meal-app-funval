f637a1890014624130b3a8e20c4c285397ee4ca7
FelipeG <felipe.guerrero.padilla@gmail.com>
2025-07-07 19:58:07 -0400

    navBar-Listo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { useState } from "react";
+import { Link } from "react-router";
 
 export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="bg-blue-600 p-4">
+    <nav className="bg-yellow-100 p-4 min-h-24 border-b border-yellow-300">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">MyApp</div>
-        <div className="space-x-4">
-          <Link to="/" className="text-white hover:text-blue-200 transition">Home</Link>
-          <Link to="/search" className="text-white hover:text-blue-200 transition">Search</Link>
-          <Link to="/categories" className="text-white hover:text-blue-200 transition">Categories</Link>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-8 text-amber-800 md:hidden cursor-pointer"
+          onClick={toggleMenu}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+          />
+        </svg>
+
+        <div className="space-x-4 hidden md:block text-yellow-800 font-bold text-lg">
+          <Link to="/" className="hover:text-yellow-500 transition">
+            Home
+          </Link>
+          <Link to="/search" className="hover:text-yellow-500 transition">
+            Search
+          </Link>
+          <Link to="/categories" className="hover:text-yellow-500 transition">
+            Categories
+          </Link>
         </div>
+        <img src="./logo.png" alt="MyApp Logo" className="size-16" />
+        <h1 className="text-yellow-800 font-bold text-2xl">Rat-Trap</h1>
       </div>
+
+      {isOpen && (
+        <div className="md:hidden mt-4 flex flex-col gap-2 text-yellow-800 font-bold text-lg">
+          <Link
+            to="/"
+            className="hover:text-yellow-500 transition"
+            onClick={() => setIsOpen(false)}
+          >
+            Home
+          </Link>
+          <Link
+            to="/search"
+            className="hover:text-yellow-500 transition"
+            onClick={() => setIsOpen(false)}
+          >
+            Search
+          </Link>
+          <Link
+            to="/categories"
+            className="hover:text-yellow-500 transition"
+            onClick={() => setIsOpen(false)}
+          >
+            Categories
+          </Link>
+        </div>
+      )}
+      
     </nav>
-  )
+  );
 }
