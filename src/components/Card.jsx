838a56ed8b457acd697f96d1f645573b58469492
TITO9538 <thomasemonzonm@gmail.com>
2025-07-08 18:00:52 -0300

    component card

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export function Card({strMeal, strMealThumb, strCategory, strArea}) {
+  return (
+    <article className='w-80 h-70 m-3 shadow-xl rounded-xl overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-xl/30'>
+        <div className='w-80 h-50 overflow-hidden flex items-center justify-center'>
+            <img src={strMealThumb} alt={strMeal} className='transition-transform duration-800 hover:scale-110'/>
+        </div>
+        <div>
+            <h3 className='text-xl font-bold pt-3 px-4 duration-500 hover:scale-105'>{strMeal}</h3>
+        </div>
+        <div className='px-5 pt-2 flex gap-2 text-xs text-gray-500'>
+            <p>{strCategory}</p>
+            <p>|</p>
+            <p>{strArea}</p>
+        </div>
+      </article>
+  )
+}
