fddc59f66e515e0a8717277a41b8d903aa8dbca1
Daniel <isral.tudela712.96@gmail.com>
2025-07-09 16:35:56 -0400

    cards y demas

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -22,14 +22,14 @@ export default function Categories() {
   }, [selectedCategory]);
 
   return (
-    <div className="px-4 py-6">
+    <div className="px-4 py-6 bg-orange-200 p-4 max-w-5xl mx-auto min-h-screen">
       {/* Carrusel de categorías */}
-      <div className="overflow-x-auto">
-        <div className="flex gap-4">
+      <div className="overflow-x-auto bg-orange-100 ">
+        <div className="flex gap-4 ">
           {categories.map((cat) => (
             <div
               key={cat.idCategory}
-              className="text-center min-w-[100px] cursor-pointer hover:scale-125 transition-transform"
+              className="text-center min-w-[100px] cursor-pointer hover:scale-125 transition-transform "
               onClick={() => setSelectedCategory(cat.strCategory)}
             >
               <img
@@ -46,20 +46,20 @@ export default function Categories() {
       {/* Mostrar comidas si hay */}
       {selectedCategory && (
   <>
-    <h2 className="text-xl font-bold mt-8 mb-4 text-center">
+    <h2 className="text-xl font-bold mt-8 mb-4 text-center ">
       Comidas de {selectedCategory}
     </h2>
-    <div className="flex flex-wrap justify-center gap-4">
+    <div className="flex flex-wrap justify-center gap-4  ">
       {meals.map((meal) => (
         <article
           key={meal.idMeal}
-          className="w-80 h-[280px] shadow-xl rounded-xl overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-xl/30 cursor-pointer"
+          className="w-80 h-[280px] shadow-xl rounded-xl overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-xl/30 cursor-pointer bg-orange-300"
         >
-          <div className="w-80 h-48 overflow-hidden flex items-center justify-center">
+          <div className="w-80 h-48 overflow-hidden flex items-center justify-center ">
             <img
               src={meal.strMealThumb}
               alt={meal.strMeal}
-              className="transition-transform duration-800 hover:scale-110 object-cover w-full h-full"
+              className="transition-transform duration-800 hover:scale-110 object-cover w-full h-full "
             />
           </div>
           <div>
